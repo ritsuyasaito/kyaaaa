58e666f3ae77f11f004c3ddb77db37fc52dc5440
【ritsuyasaito】 <ritsuya.basketball@gmail.com>
2020-02-24 12:32:45 +0900

    Second Commit

---
+++ kyaaaa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kyaaaa
+//
+//  Created by 齋藤律哉 on 2020/02/24.
+//  Copyright © 2020 ritsuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
