9d0b4c0f55bf2aa6fede0249a38181cc59556d68
【ritsuyasaito】 <ritsuya.basketball@gmail.com>
2020-02-28 16:00:08 +0900

    認証実装

---
+++ kyaaaa/Main/MailViewController.swift
@@ -0,0 +1,96 @@
+//
+//  MailViewController.swift
+//  kyaaaa
+//
+//  Created by 齋藤律哉 on 2020/02/28.
+//  Copyright © 2020 ritsuya. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class MailViewController: UIViewController {
+    
+
+   
+    enum AuthenticationState {
+        case loggedin, loggedout
+    }
+    var state: AuthenticationState = .loggedout {
+        didSet {
+      
+        }
+    }
+    var context: LAContext = LAContext()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // これを実行しないと context.biometryType が有効にならないので一度実行
+        context.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil)
+        state = .loggedout
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTabLoginButton(_ sender: Any) {
+        
+        guard state == .loggedout else {
+            self.performSegue(withIdentifier: "toUserPage", sender: nil)
+            state = .loggedout
+            return
+        }
+        
+        context = LAContext()
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil) {
+            
+            let reason = "本人認証を行います"
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, error in
+                if success {
+                    DispatchQueue.main.async { [unowned self] in
+                        self.performSegue(withIdentifier: "toUserPage", sender: nil)
+                        self.state = .loggedin
+                    }
+                } else if let laError = error as? LAError {
+                    switch laError.code {
+                    case .authenticationFailed:
+                        break
+                    case .userCancel:
+                        break
+                    case .userFallback:
+                        break
+                    case .systemCancel:
+                        break
+                    case .passcodeNotSet:
+                        break
+                    case .touchIDNotAvailable:
+                        break
+                    case .touchIDNotEnrolled:
+                        break
+                    case .touchIDLockout:
+                        break
+                    case .appCancel:
+                        break
+                    case .invalidContext:
+                        break
+                    case .notInteractive:
+                        break
+                    @unknown default:
+                        break
+                    }
+                }
+            }
+        } else {
+            // 生体認証ができない場合の認証画面表示など
+        }
+    }
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toUserPage" {
+            
+        }
+    }
+   
+
+}
