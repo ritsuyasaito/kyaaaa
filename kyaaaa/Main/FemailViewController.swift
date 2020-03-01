77ddf019e8042dd7287277d8ef76a2b7bf403f44
【ritsuyasaito】 <ritsuya.basketball@gmail.com>
2020-03-01 20:48:38 +0900

    FemailViewControllerでPostデータ取得

--- kyaaaa/Main/FemailViewController.swift
+++ kyaaaa/Main/FemailViewController.swift
@@ -8,19 +8,54 @@
 
 import UIKit
 import Firebase
+import PKHUD
 
 class FemailViewController: UIViewController {
     
     var posts = [Post]()
+    // 読み込み中かどうかを判別する変数(読み込み結果が0件の場合DZNEmptyDataSetで空の表示をさせるため)
+    var isLoading: Bool = false
+    // 下に引っ張って追加読み込みしたい場合に使う、読み込んだ投稿の最後の投稿を保存する変数
+    var lastSnapshot: DocumentSnapshot?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        loadTimeline()
     }
     
 
-    @IBAction func loadData() {
+    func loadTimeline(isAdditional: Bool = false) {
+        isLoading = true
+        Post.getAll(isAdditional: isAdditional, lastSnapshot: lastSnapshot) { (posts, lastSnapshot, error) in
+            // 読み込み完了
+            self.isLoading = false
+            self.lastSnapshot = lastSnapshot
+            //self.timelineTableView.headRefreshControl.endRefreshing()
+           // self.timelineTableView.footRefreshControl.endRefreshing()
+            
+            if let error = error {
+                print(error)
+                // エラー処理
+               // self.showError(error: error)
+                HUD.show(.error)
+            } else {
+                // 読み込みが成功した場合
+                if let posts = posts {
+                    // 追加読み込みなら配列に追加、そうでないなら配列に再代入
+                    if isAdditional == true {
+                        self.posts = self.posts + posts
+                    } else {
+                        self.posts = posts
+                        
+                    }
+                    print("成功")
+                    print(posts)
+                    
+                }
+            }
+        }
+        
         
     }
     
