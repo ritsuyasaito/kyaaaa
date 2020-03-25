c5190ec51334cbf5b563e6d84f9ab3a69cb493a7
EricaFujita <46892139+EricaFujita@users.noreply.github.com>
2020-03-25 09:13:41 +0900

    メール認証

--- kyaaaa/Login/SignUpViewController.swift
+++ kyaaaa/Login/SignUpViewController.swift
@@ -9,15 +9,18 @@
 import UIKit
 import PKHUD
 import SwiftMessages
+import Firebase
+import FirebaseAuth
+import SCLAlertView
 
 class SignUpViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         emailTextField.delegate = self
         passwordTextField.delegate = self
@@ -28,39 +31,57 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func signUp() {
-            guard let email = emailTextField.text else { return }
-            guard let password = passwordTextField.text else { return }
-            
-            
-            UserModel.signUp(email: email, password: password) { (error) in
-               
-                if let error = error {
-                  print(error)
-                   
-                  let view = MessageView.viewFromNib(layout: .messageView)
-                  view.configureTheme(.error)
-                  view.button?.isHidden = true
-                  view.titleLabel?.text = "エラー"
-                  view.bodyLabel?.text = "アドレスを確認してください。または、パスワードを6文字以上で複雑化してください。"
-                  SwiftMessages.show(view: view)
-                } else {
-                    HUD.flash(.success, delay: 1.0)
-                    
-                    let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-                    let rootViewController = storyboard.instantiateViewController(withIdentifier: "First")
-                    UIApplication.shared.keyWindow?.rootViewController = rootViewController
-                    
-                    //ログイン状態の保持
-                    let ud = UserDefaults.standard
-                    ud.set(true, forKey: "isLogin")
-                    ud.synchronize()
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+//        UserModel.signUp(email: email, password: password) { (error) in
+//            if let error = error {
+//                print(error)
+//                let view = MessageView.viewFromNib(layout: .messageView)
+//                view.configureTheme(.error)
+//                view.button?.isHidden = true
+//                view.titleLabel?.text = "エラー"
+//                view.bodyLabel?.text = "アドレスを確認してください。または、パスワードを6文字以上で複雑化してください。"
+//                SwiftMessages.show(view: view)
+//            } else {
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    if let error = error {
+                        print(error)
+                        
+                        let view = MessageView.viewFromNib(layout: .messageView)
+                        view.configureTheme(.error)
+                        view.button?.isHidden = true
+                        view.titleLabel?.text = "エラー"
+                        view.bodyLabel?.text = "アドレスを確認してください。または、パスワードを6文字以上で複雑化してください。"
+                        SwiftMessages.show(view: view)
+                        return
+                    }else{
+                        result?.user.sendEmailVerification(completion: { (error) in
+                            if error == nil{
+                                let appearance = SCLAlertView.SCLAppearance(
+                                    showCloseButton: false
+                                )
+                                let alert = SCLAlertView(appearance: appearance)
+                                alert.addButton("はい") {
+                                    let storyboard = UIStoryboard(name: "Login", bundle: Bundle.main)
+                                    let rootViewController = storyboard.instantiateViewController(withIdentifier: "Login")
+                                    UIApplication.shared.keyWindow?.rootViewController = rootViewController
+                                    
+                                }
+                                alert.showInfo("", subTitle: "メール認証を行ってください")
+                            }
+                        })
+                        
+                    }
                 }
-            }
-        }
-    
-    
-
-    
-    
-
+//            }
+//        }
+    }
 }
+
+
+
+
+
+
+
