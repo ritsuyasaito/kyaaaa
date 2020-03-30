23384777f7b9807758a33cd06872d8b24e4269ad
EricaFujita <46892139+EricaFujita@users.noreply.github.com>
2020-03-31 00:42:24 +0900

    kyaaa

--- kyaaaa/launchViewController.swift
+++ kyaaaa/launchViewController.swift
@@ -10,19 +10,18 @@ import UIKit
 
 class launchViewController: UIViewController {
     
-    var logoImageView: UIImageView!
+    var logoImageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = UIColor.white
+        self.view.backgroundColor = UIColor.black
 
         //imageView作成
-        //self.logoImageView = UIImageView(frame: CGRectMake(0, 0, 204, 77))
-        //画面centerに
+self.logoImageView = UIImageView(frame: CGRect(x: self.view.bounds.width/2, y: self.view.bounds.height/2, width: 350.0, height: 350.0))        //画面centerに
         self.logoImageView.center = self.view.center
         //logo設定
-        self.logoImageView.image = UIImage(named: "logo")
+        self.logoImageView.image = UIImage(named: "kyaaaa_black.jpg")
         //viewに追加
         self.view.addSubview(self.logoImageView)
     }
@@ -39,14 +38,16 @@ class launchViewController: UIViewController {
         })
 
         //拡大させて、消えるアニメーション
-        UIView.animate(withDuration: 0.2,
-            delay: 1.3,
+        UIView.animate(withDuration: 0.3,
+            delay: 1.0,
             options: UIView.AnimationOptions.curveEaseOut,
             animations: { () in
-                self.logoImageView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                self.logoImageView.transform = CGAffineTransform(scaleX: 7.0, y: 7.0)
                 self.logoImageView.alpha = 0
             }, completion: { (Bool) in
                 self.logoImageView.removeFromSuperview()
+                self.performSegue(withIdentifier: "toStart", sender: nil)
+
         })
     }
     
