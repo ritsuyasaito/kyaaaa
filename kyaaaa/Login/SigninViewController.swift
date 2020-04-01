4dcbd3a74f80f417177be593b5320cb07fdba4e8
ritsuyasaito <49610071+ritsuyasaito@users.noreply.github.com>
2020-04-01 22:12:17 +0900

    Revert "Debug"

--- kyaaaa/Login/SigninViewController.swift
+++ kyaaaa/Login/SigninViewController.swift
@@ -35,8 +35,9 @@ class SigninViewController: UIViewController, LoginButtonDelegate{
 //        　fbLoginButton.frame = CGRect(x: view.frame.size.width / 2 - view.frame.size.width / 4, y: view.frame.size.height / 4, width: view.frame.size.width / 2, height: 660)
         fbLoginButton.frame = CGRect(x: 32, y: 650, width: 351, height: 41)
 
-
     //fbLoginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+
+
 
 
 
